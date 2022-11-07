d5f1723efc4773da572043717ae8c902a87957cb
HAIURA Andreea-Isabela <andreea_haiura@yahoo.com>
2022-11-07 23:33:28 +0200

    update patient entity

--- MyDocAppointment.BusinessLayer/Data/MyDocAppointmentDatabaseContext.cs
+++ MyDocAppointment.BusinessLayer/Data/MyDocAppointmentDatabaseContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MyDocAppointment.BusinessLayer.Entities;
 
 namespace MyDocAppointment.BusinessLayer.Data
 {
     public class MyDocAppointmentDatabaseContext : DbContext
     {
+
+        public DbSet<Patient> Patients { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source = MyDocAppointmentManagement.db");
---
+++ MyDocAppointment.BusinessLayer/Repositories/PatientRepository.cs
@@ -0,0 +1,48 @@
+﻿using MyDocAppointment.BusinessLayer.Data;
+using MyDocAppointment.BusinessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDocAppointment.BusinessLayer.Repositories
+{
+    public class PatientRepository : IPatientRepository
+    {
+        private readonly MyDocAppointmentDatabaseContext context;
+
+        public PatientRepository(MyDocAppointmentDatabaseContext context)
+        {
+            this.context = context;
+        }
+
+        public Patient? GetById(int id)
+        {
+            return this.context.Patients.FirstOrDefault(patient => patient.Id == id);
+        }
+
+        public IEnumerable<Patient> GetAll()
+        {
+            return this.context.Patients.ToList();
+        }
+
+        public void Add(Patient patient)
+        {
+            this.context.Patients.Add(patient);
+            this.context.SaveChanges();
+        }
+        public void Update(Patient patient)
+        {
+            this.context.Update(patient);
+            this.context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var patient = this.context.Patients.FirstOrDefault(patient => patient.Id, id);
+            this.context.Patients.Remove(patient);
+            this.context.Patients.Remove(GetById(id));
+        }
+    }
+}
