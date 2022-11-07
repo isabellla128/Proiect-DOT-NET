8d2440b46d0599833570150238fb598778d79493
Madalina-Carausu <madalinacarausu@gmail.com>
2022-11-07 23:28:27 +0200

    Add prescription and medication entities

---
+++ MyDocAppointment.BusinessLayer/Entities/Medication.cs
@@ -0,0 +1,18 @@
+﻿namespace MyDocAppointment.BusinessLayer.Entities
+{
+    public class Medication
+    {
+        public Medication(string name, int stock = 0)
+        {
+            Name = name;
+            Stock = stock;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Stock { get; set; }
+
+    }
+}
