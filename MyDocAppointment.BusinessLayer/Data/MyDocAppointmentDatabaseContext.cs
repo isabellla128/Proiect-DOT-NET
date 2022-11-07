c83d18685ac2c65267c3b5529b4a8bad4f800a62
HAIURA Andreea-Isabela <andreea_haiura@yahoo.com>
2022-11-07 23:18:29 +0200

    main
    main

---
+++ MyDocAppointment.BusinessLayer/Data/MyDocAppointmentDatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyDocAppointment.BusinessLayer.Data
+{
+    public class MyDocAppointmentDatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = MyDocAppointmentManagement.db");
+        }
+    }
+}
