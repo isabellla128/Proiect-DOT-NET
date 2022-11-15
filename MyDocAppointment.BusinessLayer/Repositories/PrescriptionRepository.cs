89927652b8c7ae3f03d084ec90137f581227fc43
MUNTEAN Iulian <75438799+iulianmtn@users.noreply.github.com>
2022-11-15 17:24:16 +0200

    remove unnecessary usings

--- MyDocAppointment.BusinessLayer/Repositories/PrescriptionRepository.cs
+++ MyDocAppointment.BusinessLayer/Repositories/PrescriptionRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MyDocAppointment.BusinessLayer.Data;
+﻿using MyDocAppointment.BusinessLayer.Data;
 using MyDocAppointment.BusinessLayer.Entities;
 
 namespace MyDocAppointment.BusinessLayer.Repositories
