b1444927a9ac39263b8d2b053d4c314a4f6fd22c
Madalina-Carausu <madalinacarausu@gmail.com>
2022-11-21 21:49:40 +0200

    Add schedule api, event api and appointment api

---
+++ MyDocAppointment.API/Features/Schedules/ScheduleDto.cs
@@ -0,0 +1,11 @@
+﻿using MyDocAppointment.BusinessLayer.Entities;
+
+namespace MyDocAppointment.API.Features.Schedules
+{
+    public class ScheduleDto
+    {
+        public Guid Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
