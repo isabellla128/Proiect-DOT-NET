2889c4947819afe7a704056e0ae63246a5a2b5a5
Radu Chelaru <raduchelaru1@gmail.com>
2022-11-22 12:33:37 +0200

    Create MedicationsControllerTests.cs
    Medication test

---
+++ MyDocAppointment.API.Tests/MedicationsControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using MyDocAppointment.API.Features.Medications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyDocAppointment.API.Tests
+{
+    public class MedicationsControllerTests : BaseIntegrationTests<MedicationsController>
+    {
+        private const string ApiURL = "v1/api/Medications";
+
+        [Fact]
+        public async void When_CreatedMedication_Then_ShouldReturnMedicationInTheGetRequest()
+        {
+            MedicationDto medicationDto = createSUT();
+            // Act
+            var createMedicationResponse = await HttpClient.PostAsJsonAsync(ApiURL, medicationDto);
+            var getMedicationResponse = await HttpClient.GetAsync(ApiURL);
+            // Assert
+            createMedicationResponse.EnsureSuccessStatusCode();
+            createMedicationResponse.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+
+            getMedicationResponse.EnsureSuccessStatusCode();
+            var medications = await getMedicationResponse.Content.ReadFromJsonAsync<List<MedicationDto>>();
+            medications.Should().HaveCount(1);
+            medications.Should().NotBeNull();
+        }
+
+        private static MedicationDto createSUT()
+        {
+            // Arrange
+            return new MedicationDto
+            {
+                Name = "Paracetamol",
+                Stock = 99
+               
+            };
+        }
+    }
+}
