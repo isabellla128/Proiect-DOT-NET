80791c7d421b87acb5aafb90f93d4bef56361825
Radu Chelaru <raduchelaru1@gmail.com>
2022-11-28 21:35:07 +0200

    Adding doctors and medications

---
+++ ClientApp/src/app/services/medication.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BASE_API_URL } from 'src/global';
+import { Medication } from 'src/models/medication';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MedicationService {
+  url = BASE_API_URL + 'Medications';
+  constructor(private http: HttpClient) {}
+
+  getAllMedications(): Observable<Medication[]> {
+    return this.http.get<Medication[]>(this.url);
+  }
+}
---
+++ ClientApp/src/global.ts
@@ -0,0 +1,1 @@
+export const BASE_API_URL = 'https://localhost:7260/v1/api/';
