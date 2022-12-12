7ac2876f292795d365453bc57e5d5e6356bc16a1
isabellla128 <andreea_haiura@yahoo.com>
2022-12-12 11:41:00 +0200

    FirstName from DoctorDto shoul be nullable

--- DOTNET/MyDocAppointment.API/Features/Doctors/CreateDoctorDto.cs
+++ DOTNET/MyDocAppointment.API/Features/Doctors/CreateDoctorDto.cs
@@ -2,7 +2,7 @@
 {
     public class CreateDoctorDto
     {
-        public string FirstName { get; set; }
+        public string? FirstName { get; set; }
         public string LastName { get; set; }
         public string Specialization { get; set; }
         public string Email { get; set; }
