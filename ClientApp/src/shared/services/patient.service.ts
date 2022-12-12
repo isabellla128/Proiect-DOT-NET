6a96afc22f760f435542461f3114884adde50109
Radu Chelaru <raduchelaru1@gmail.com>
2022-12-12 18:22:51 +0200

    Patient service

--- ClientApp/src/shared/services/patient.service.ts
+++ ClientApp/src/shared/services/patient.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { BASE_API_URL } from 'src/environments/global';
+import { Patient } from 'src/models/patent';
+import AbstractRestService from '../abstracts/AbstractRestService';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class PatientService {
-
-  constructor() { }
+export class PatientService extends AbstractRestService<Patient> {
+  patientId: String = '5049a678-9524-48df-99f2-cecf3e105b83';
+  constructor(private http: HttpClient) {
+    super(http, BASE_API_URL + 'Patients', new BehaviorSubject<Patient[]>([]));
+  }
 }
