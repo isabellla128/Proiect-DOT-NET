ff1a72cf6ec7531bd2223226294696201a73d8b4
MUNTEAN Iulian <75438799+iulianmtn@users.noreply.github.com>
2022-12-12 23:53:24 +0200

    patient and doctor id back in appointment dto

--- DOTNET/MyDocAppointment.API/Features/Appointments/AppointmentDto.cs
+++ DOTNET/MyDocAppointment.API/Features/Appointments/AppointmentDto.cs
@@ -5,8 +5,11 @@ namespace MyDocAppointment.API.Features.Appointments
     public class AppointmentDto
     {
         public Guid Id { get; set; }
+        public Guid DoctorId { get; set; }
 
         public Doctor? Doctor { get; set; }
+
+        public Guid PatientId { get; set; }
 
         public Patient? Patient { get; set; }
 
