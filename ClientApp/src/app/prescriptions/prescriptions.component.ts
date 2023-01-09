921946ac0dc8d20023e50be4d104b833b6dc6c96
Radu Chelaru <raduchelaru1@gmail.com>
2023-01-09 13:41:31 +0200

    adding prescription to cart

--- ClientApp/src/app/prescriptions/prescriptions.component.ts
+++ ClientApp/src/app/prescriptions/prescriptions.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Doctor } from 'src/models/doctor';
+import { Medication, MedicationDosages } from 'src/models/medication';
 import { Patient } from 'src/models/patient';
 import { Prescription } from 'src/models/prescription';
 import { DoctorService } from 'src/shared/services/doctor.service';
 import { MedicationService } from 'src/shared/services/medication.service';
 import { PatientService } from 'src/shared/services/patient.service';
 import { PrescriptionService } from 'src/shared/services/prescription.service';
+import { ShoppingCartService } from 'src/shared/services/shopping-cart.service';
 
 @Component({
   selector: 'app-prescriptions',
@@ -17,10 +19,11 @@ export class PrescriptionsComponent implements OnInit {
   doctors: Doctor[] = [];
   patients: Patient[] = [];
   constructor(
+    public medicationService: MedicationService,
     private prescriptionService: PrescriptionService,
     private doctorService: DoctorService,
-    public medicationService: MedicationService,
-    private patientService: PatientService
+    private patientService: PatientService,
+    private shoppingCartService: ShoppingCartService
   ) {}
 
   ngOnInit() {
@@ -34,13 +37,19 @@ export class PrescriptionsComponent implements OnInit {
       this.patientService.collection$.subscribe(
         (patients) => (this.patients = patients)
       );
-      console.log(prescriptions);
-      console.log(this.doctors);
     });
   }
 
   getDate(date: string) {
     const dateObj = new Date(date);
     return dateObj.toLocaleDateString('en-GB');
+  }
+
+  addToCart(dosages: MedicationDosages[] | undefined) {
+    dosages?.forEach((dosage) => {
+      const medication = this.medicationService.getOne(dosage.medicationId);
+      for (let index = 0; index < dosage.quantity; index++)
+        this.shoppingCartService.addToCard(medication || ({} as Medication));
+    });
   }
 }
