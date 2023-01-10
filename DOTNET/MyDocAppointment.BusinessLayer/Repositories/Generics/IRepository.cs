a5a8669106ffe63bc9bef68d541d2850353757ec
Madalina-Carausu <madalinacarausu@gmail.com>
2023-01-10 14:55:57 +0200

    Update IRepository.cs

--- DOTNET/MyDocAppointment.BusinessLayer/Repositories/Generics/IRepository.cs
+++ DOTNET/MyDocAppointment.BusinessLayer/Repositories/Generics/IRepository.cs
@@ -8,9 +8,9 @@ namespace MyDocAppointment.BusinessLayer.Repositories
         Task<T?> Delete(Guid id);
         Task<IReadOnlyCollection<T>> GetAll();
         Task<T?> GetById(Guid id);
+        Task<T?> GetById(string id);
         T Update(T entity);
         Task<IReadOnlyCollection<T>> Find(Expression<Func<T, bool>> predicate);
         void SaveChanges();
-        Task<T?> GetById(string id);
     }
 }
